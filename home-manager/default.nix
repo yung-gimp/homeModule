369c2d4f8e1dd085f1e8e8cc6250bf8bfe9a0f26
yung-gimp <>
2025-07-09 18:44:47 -0400

    basic userspace

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,10 +1,19 @@
 { pkgs, inputs, ... }:
 {
-  programs.home-manager.enable = true;
   home = {
     packages = [ pkgs.htop ];
     stateVersion = "24.11";
+  }; 
+
+  programs = {
+    home-manager.enable = true;
+    firefox.enable = true;
+    foot.enable = true;
+    git.enable = true;
+    neovim.enable = true;
+    rofi.enable = true;
   };
+
   imports = [
     inputs.impermanence.homeManagerModules.impermanence
     ./hyprland
--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -1,3 +1,20 @@
+let
+
+  mod = "SUPER";
+
+in
+
+
 {
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      bind = [
+        "${mod}, C, killactive,"
+        "${mod}, M, exec, hyprctl dispatch exit"
+        "${mod}, Q, exec, foot"
+        "${mod}, R, exec, rofi -show drun"
+      ];
+    };
+  };
 }
